386d9c2246626e6e530d5c5a8565de16327d161a
Lisa <misslisazhou@gmail.com>
2017-03-05 12:30:09 -1000

    retrive values from input to calculate sizes, check for erros as well

--- pipes.js
+++ pipes.js
@@ -29,7 +29,7 @@ function pipeSize(rate) {
 function valveSize(rate) {
   var valve = Math.round(rate * 1.25 * 1.25 / 5) * 5;
   if (valve < 15 || valve > 100){
-    throw new Error("Sorry, valve size not available for the given flow rate.")
+    throw new Error("Sorry, valve sizes are not available for the given flow rates. Please try again with alternative flow rates.")
   }else{
     return valve;
   }
@@ -38,3 +38,18 @@ function valveSize(rate) {
 var pipeSizes = [8,10,12,16,20,25,32,40,50,63,80,100,127,160,201,254,320,404,509,642,810,1021,1288,1624,2048,2582,3256,4106,5178,6529,8233,10382,13091,16507,20815,26248,33098,41735,52627,66361,83680,105518,133056,167780,211566,250000,300000,350000,400000,450000,500000,600000,700000,750000,800000,900000,1000000,1250000,1500000,1750000,2000000]
 
 var valveSizes = [15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]
+
+var calculate = document.getElementById('calculate');
+calculate.addEventListener('click', runProgram);
+var output = document.getElementById('output');
+function runProgram() {
+  try{
+    var xInput = document.getElementById('flow-rate-x').value;
+    var yInput = document.getElementById('flow-rate-y').value;
+    var results = calculateAllSizes(xInput, yInput);
+    output.innerHTML = "Pipe X Size: " + results.pipeSizeX + " " + results.valveSizeX ;
+  }
+  catch(err){
+    output.innerHTML = err.message;
+  }
+}
