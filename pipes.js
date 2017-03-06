297c8d21d318b040dae27cbde79ae57c9522eff8
Lisa <misslisazhou@gmail.com>
2017-03-05 15:44:57 -1000

    cleaned up code, add conditional to check if input is a number

--- pipes.js
+++ pipes.js
@@ -29,7 +29,7 @@ function pipeSize(rate) {
 function valveSize(rate) {
   var valve = Math.round(rate * 1.25 * 1.25 / 5) * 5;
   if (valve < 15 || valve > 100){
-    throw new Error("Sorry, valve sizes are not available for the given flow rates. Please try again with alternative flow rates.")
+    throw new Error('Sorry, valve sizes are not available for the given flow rates. Please try again with alternative flow rates.')
   }else{
     return valve;
   }
@@ -50,37 +50,40 @@ var v1 = document.getElementById('v1');
 var v2 = document.getElementById('v2');
 var errorBox = document.getElementById('error-container');
 function runProgram() {
-  errorBox.innerHTML = "";
-  xPipe.innerHTML = "<strong>X Pipe Size:</strong> _____";
-  yPipe.innerHTML = "<strong>Y Pipe Size:</strong> _____";
-  xValve.innerHTML = "<strong>X Valve Size:</strong> _____";
-  yValve.innerHTML = "<strong>Y Valve Size:</strong> _____";
-  zPipe.innerHTML = "<strong>Z Pipe Size:</strong> _____";
-  v1.style.width = "50px";
-  v1.style.height = "50px";
-  v1.style.top = "77.5px";
-  v1.style.left = "75px";
-  v2.style.width = "50px";
-  v2.style.height = "50px";
-  v2.style.top = "172.5px";
-  v2.style.left = "75px";
+  errorBox.innerHTML = '';
+  xPipe.innerHTML = '<strong>X Pipe Size:</strong> _____';
+  yPipe.innerHTML = '<strong>Y Pipe Size:</strong> _____';
+  xValve.innerHTML = '<strong>X Valve Size:</strong> _____';
+  yValve.innerHTML = '<strong>Y Valve Size:</strong> _____';
+  zPipe.innerHTML = '<strong>Z Pipe Size:</strong> _____';
+  v1.style.width = '50px';
+  v1.style.height = '50px';
+  v1.style.top = '77.5px';
+  v1.style.left = '75px';
+  v2.style.width = '50px';
+  v2.style.height = '50px';
+  v2.style.top = '172.5px';
+  v2.style.left = '75px';
   try{
     var xInput = document.getElementById('flow-rate-x').value;
     var yInput = document.getElementById('flow-rate-y').value;
+    if (isNaN(xInput)){
+      throw new Error('Please input numbers for calculation.')
+    }
     var results = calculateAllSizes(xInput, yInput);
-    xPipe.innerHTML = "<strong>X Pipe Size:</strong> " + results.pipeSizeX;
-    yPipe.innerHTML = "<strong>Y Pipe Size:</strong> " + results.pipeSizeY;
-    xValve.innerHTML = "<strong>X Valve Size:</strong> " + results.valveSizeX;
-    v1.style.width = results.valveSizeX + "px";
-    v1.style.height = results.valveSizeX + "px";
-    v1.style.top = 100 - results.valveSizeX/2 + 2.5 + "px";
-    v1.style.left = 100 - results.valveSizeX/2 + "px";
-    v2.style.width = results.valveSizeY + "px";
-    v2.style.height = results.valveSizeY + "px";
-    v2.style.top = 200 - results.valveSizeY/2 - 2.5 + "px";
-    v2.style.left = 100 - results.valveSizeY/2 + "px";
-    yValve.innerHTML = "<strong>Y Valve Size:</strong> " + results.valveSizeY;
-    zPipe.innerHTML = "<strong>Z Pipe Size:</strong> " + results.pipeSizeZ;
+    xPipe.innerHTML = '<strong>X Pipe Size:</strong> ' + results.pipeSizeX;
+    yPipe.innerHTML = '<strong>Y Pipe Size:</strong> ' + results.pipeSizeY;
+    xValve.innerHTML = '<strong>X Valve Size:</strong> ' + results.valveSizeX;
+    v1.style.width = results.valveSizeX + 'px';
+    v1.style.height = results.valveSizeX + 'px';
+    v1.style.top = 100 - results.valveSizeX/2 + 2.5 + 'px';
+    v1.style.left = 100 - results.valveSizeX/2 + 'px';
+    v2.style.width = results.valveSizeY + 'px';
+    v2.style.height = results.valveSizeY + 'px';
+    v2.style.top = 200 - results.valveSizeY/2 - 2.5 + 'px';
+    v2.style.left = 100 - results.valveSizeY/2 + 'px';
+    yValve.innerHTML = '<strong>Y Valve Size:</strong> ' + results.valveSizeY;
+    zPipe.innerHTML = '<strong>Z Pipe Size:</strong> ' + results.pipeSizeZ;
   }
   catch(err){
     errorBox.innerHTML = err.message;
